9fdfc272b0fd19af5795bd6c1b29cf7b6084c1b7
MarcoFavorito <marco.favorito@gmail.com>
2020-12-06 23:07:46 +0100

    fix poetry deps install script

--- scripts/install-poetry-dependencies.sh
+++ scripts/install-poetry-dependencies.sh
@@ -4,9 +4,7 @@ set -e
 
 function install_poetry_dependency(){
     current_directory="$(pwd)"
-    cd "$1"
-    poetry build
-    pip install dist/*.whl
+    cd "$1" && poetry build && pip install "$(find dist -type f -name "*.whl")"
     cd "${current_directory}"
 }
 
