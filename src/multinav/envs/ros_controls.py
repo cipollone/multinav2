3b240cd8a064908c7992e90ce5afabfbcfec81f3
Roberto Cipollone <cipollone.rt@gmail.com>
2020-12-07 16:44:07 +0100

    Complete Gym env.
    + The reward and end-of-episode make no sense, but the structure is
      complete.

--- src/multinav/envs/ros_controls.py
+++ src/multinav/envs/ros_controls.py
@@ -37,7 +37,7 @@ class RosControlsEnv(gym.Env):
     https://github.com/iocchi/StageROSGym. See that repo to setup also the
     other side.
     """
-    
+
     # NOTE: many settings depend on the communication protocol.
     #   Make sure that the other side respects the same actions, signals,
     #   and observation space.
@@ -50,9 +50,8 @@ class RosControlsEnv(gym.Env):
     # Communication protocol
     actions_port = 30005
     states_port = 30006
-    state_msg_len = 20    # a numpy vector of 5 float32
-    action_msg_len = 4    # a positive numpy scalar of type int32
-
+    state_msg_len = 20  # a numpy vector of 5 float32
+    action_msg_len = 4  # a positive numpy scalar of type int32
 
     class StateReceiver(Receiver):
         """Just a wrapper that deserializes state vectors."""
@@ -71,7 +70,6 @@ class RosControlsEnv(gym.Env):
             array = np.frombuffer(buff, dtype=np.float32)
             return array
 
-
     class ActionSender(Sender):
         """Just a wrapper that serializes actions."""
 
@@ -88,39 +86,42 @@ class RosControlsEnv(gym.Env):
             # Send
             Sender.send(self, buff)
 
-
     def __init__(self, n_actions):
         """Initialize.
 
-        :param n_actions: the number of action allowed from the 
+        :param n_actions: the number of action allowed from the
             remote ROS Controller.
         """
 
         # Define spaces
         self.action_space = gym.spaces.Discrete(n_actions)
         self.observation_space = gym.spaces.Box(
-            low=float("-inf"), high=float("inf"), shape=[5], dtype=np.float32)
-            # NOTE: Actually the third is an angle
+            low=float("-inf"), high=float("inf"), shape=[5], dtype=np.float32
+        )
+        # NOTE: Actually the third is an angle
 
         # Initialize connections
         self.action_sender = RosControlsEnv.ActionSender(
-            msg_length=self.action_msg_len, port=self.actions_port, wait=True,
+            msg_length=self.action_msg_len,
+            port=self.actions_port,
+            wait=True,
         )
         self.state_receiver = RosControlsEnv.StateReceiver(
-            msg_length=self.state_msg_len, ip="localhost",
-            port=self.states_port, wait=True,
+            msg_length=self.state_msg_len,
+            ip="localhost",
+            port=self.states_port,
+            wait=True,
         )
 
         # Connect now
         self.action_sender.start()
         print("> Serving actions on", self.action_sender.server.server_address)
         print("> Connecting to ", self.state_receiver.ip, ":",
-            self.state_receiver.port, " for states. (pause)",
-            sep="", end=" ",
+              self.state_receiver.port, " for states. (pause)",
+              sep="", end=" "
         )
         input()
         self.state_receiver.start()
-
 
     def reset(self):
         """Reset the environment to the initial state.
@@ -128,13 +129,16 @@ class RosControlsEnv(gym.Env):
         :return: The initial observation.
         """
 
+        # Episode vars
+        self._time = 0
+
         # Send signal
-        self.action_sender.send(
-            self._signals["reset"]
-        )
+        self.action_sender.send(self._signals["reset"])
 
-        # TODO: return initial state
+        # Return initial observation
+        observation = self.state_receiver.receive()
 
+        return observation
 
     def step(self, action):
         """Run one timestep of the environment's dynamics.
@@ -151,24 +155,31 @@ class RosControlsEnv(gym.Env):
         if not 0 <= action < self.action_space.n:
             raise RuntimeError(str(action) + " is not an action.")
 
+        self._time += 1
+
         # Execute
         self.action_sender.send(action)
 
         # Read observation
         observation = self.state_receiver.receive()
 
-        # TODO: compute reward and done according to a criterion implemented
-        #   in some external class
-        return (observation, 0.0, False, {})
+        # Compute reward and end
+        reward = self.reward_at_state(observation)
+        done = self.is_episode_done()
 
+        # Diagnostics
+        info = {"time": self._time}
+
+        return (observation, reward, done, info)
+
+    # TODO: the methods below are temporary placeholders used for development
 
     @staticmethod
     def interactive_test():
-        """Demonstrates that connection works."""
-        # TODO: later, delete this method
+        """Demonstrates that connection works: this can be deleted."""
 
         # Instantiate
-        ros_env = RosControlsEnv(5)   # Five actions on iocchi/StageROSGym@1bda032
+        ros_env = RosControlsEnv(5)  # Five actions on iocchi/StageROSGym@1bda032
 
         ros_env.reset()
 
@@ -180,4 +191,15 @@ class RosControlsEnv(gym.Env):
             else:
                 obs, reward, done, info = ros_env.step(action)
 
-            print(obs)
+            print(obs, reward, done, info)
+
+    def reward_at_state(self, observation):
+        """Returns the reward resulting from one observation of the robot."""
+
+        # Proportional to x because we want it to go left
+        return -observation[0]
+
+    def is_episode_done(self):
+        """Simple criterion for episode termination."""
+
+        return self._time > 30
