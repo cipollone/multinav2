daa5db3a79e0eff8fa7a83c9009af95f94379976
Roberto Cipollone <cipollone.rt@gmail.com>
2021-03-30 17:55:39 +0000

    Saving experiments setup.

--- scripts/runnext.sh
+++ scripts/runnext.sh
@@ -1,17 +1,17 @@
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 1 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 1 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 2 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 2 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 3 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 3 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 4 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 4 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 5 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 5 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 6 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 6 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 7 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 7 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 8 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 8 &
 sleep 4s
-python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 9
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-params.json --id 9
