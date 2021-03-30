b2068dbbf2371b3947b9ba0e79c7cc38eedbc444
Roberto Cipollone <cipollone.rt@gmail.com>
2021-03-30 12:24:09 +0000

    Small updates.

---
+++ scripts/runnext.sh
@@ -0,0 +1,9 @@
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 1 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 2 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 3 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 4 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 5 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 6 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 7 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 8 &
+python -m multinav train --env sapientino-grid --params inputs/sapientino-grid-noshaping-params.json --id 9
