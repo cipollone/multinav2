f8bdc732c38a75aa3fdbadb7168e3bb30fcc0739
Roberto Cipollone <cipollone.rt@gmail.com>
2021-12-10 19:35:38 +0100

    Important bugfix in room state mapping.

--- multinav/envs/env_grid_rooms.py
+++ multinav/envs/env_grid_rooms.py
@@ -21,16 +21,16 @@
 #
 """Reward shaping wrapper."""
 import logging
-from typing import Any, Mapping, Optional, Tuple
+from typing import Any, List, Mapping, Optional, Sequence, Tuple
 
 from gym.wrappers import TimeLimit
 from gym_sapientino import SapientinoDictSpace
 from gym_sapientino.core import actions, configurations
-from gym_sapientino.core.types import Colors
-from gym_sapientino.core.types import id2color as room2color
+from gym_sapientino.core.types import Colors, color2id, id2color
 from temprl.types import Interpretation
 
 from multinav.algorithms.agents import QFunctionModel
+from multinav.envs.env_abstract_rooms import AbstractRooms
 from multinav.envs.temporal_goals import FluentExtractor, with_nonmarkov_rewards
 from multinav.helpers.reward_shaping import StateH, ValueFunctionRS
 from multinav.wrappers.reward_shaping import RewardShapingWrapper
@@ -57,7 +57,7 @@ class GridRoomsFluents(FluentExtractor):
         charset -= {'|', '#', ' ', '\n', '\r'}
         self.rooms = charset
         self.n_rooms = len(charset)
-        self.fluents = {str(room2color[room]) for room in self.rooms}
+        self.fluents = {str(id2color[room]) for room in self.rooms}
 
     @property
     def all(self):
@@ -78,17 +78,35 @@ class GridRoomsFluents(FluentExtractor):
         return {color}
 
 
-def grid_to_abs(obs: Mapping[str, Any]):
+class Grid2Abs:
     """Transform features of grid_rooms to AbstractRooms."""
-    color = int2color[obs["color"]]
-    assert color not in ("blank", "wall"), (
-        "Colors should be everywhere because they fill rooms")
-    abs_state = obs["color"] - 2  # blank and wall come first
-    assert 0 <= abs_state
-    return abs_state
+
+    def __init__(self, rooms_connectivity: Sequence[Sequence[str]]):
+        """Initialize.
+
+        :param rooms_connectivity: see AbstractRooms.
+        """
+        self._abs_env = AbstractRooms(
+            rooms_connectivity=rooms_connectivity,
+            initial_room=rooms_connectivity[0][0],
+            p_failure=0.0,
+        )
+
+    def __call__(self, obs: Mapping[str, Any]):
+        """Convert to abstract."""
+        color = int2color[obs["color"]]
+        assert color not in ("blank", "wall"), (
+            "Colors should be everywhere because they fill rooms")
+        abs_state = self._abs_env._room2id[color2id[Colors(color)]]
+        return abs_state
 
 
-def abs_rooms_shaper(path: str, gamma: float, return_invariant: bool) -> ValueFunctionRS:
+def abs_rooms_shaper(
+    path: str,
+    gamma: float,
+    return_invariant: bool,
+    rooms_connectivity: List[List[str]],
+) -> ValueFunctionRS:
     """Define a reward shaper on the previous environment.
 
     This loads a saved agent for `AbstractRooms` then it uses it to
@@ -98,10 +116,14 @@ def abs_rooms_shaper(path: str, gamma: float, return_invariant: bool) -> ValueFu
     :param gamma: discount factor to apply for shaping.
     :param return_invariant: if true, we apply classic return-invariant reward shaping.
         We usually want this to be false.
+    :param rooms_connectivity: see AbstractRooms.
     :return: reward shaper to apply.
     """
     # Trained agent on abstract environment
     agent = QFunctionModel.load(path=path)
+
+    # Map
+    grid_to_abs = Grid2Abs(rooms_connectivity=rooms_connectivity)
 
     def map_with_temporal_goals(state: Tuple[Mapping[str, Any], list]) -> StateH:
         obs = grid_to_abs(state[0])
@@ -166,6 +188,7 @@ def make(params: Mapping[str, Any], log_dir: Optional[str] = None):
             path=params["shaping"],
             gamma=params["shaping_gamma"],
             return_invariant=params["return_invariant"],
+            rooms_connectivity=params["rooms_connectivity"],
         )
         env = RewardShapingWrapper(env, reward_shaper=abs_shaper)
 
